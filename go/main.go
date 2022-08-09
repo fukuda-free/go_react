d87b5cae77ad2569583e63d4b20ca670dc5ac41b
fukuda.hiroaki <fukuda.hiroaki@k-idea.jp>
2022-08-09 13:29:31 +0900

    作成途中

--- go/main.go
+++ go/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"qiita/controller"
+	"qiita/middleware"
+)
 
 func main() {
-    fmt.Println("Hello, world")
+	engine := gin.Default()
+	// ミドルウェア
+	engine.Use(middleware.RecordUaAndTime)
+	// CRUD 書籍
+	bookEngine := engine.Group("/book")
+	{
+		v1 := bookEngine.Group("/v1")
+		{
+			v1.POST("/add", controller.BookAdd)
+			v1.GET("/list", controller.BookList)
+			v1.PUT("/update", controller.BookUpdate)
+			v1.DELETE("/delete", controller.BookDelete)
+		}
+	}
+	engine.Run(":3000")
 }
